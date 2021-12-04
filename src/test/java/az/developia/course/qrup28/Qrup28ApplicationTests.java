6096c507af25a5693f1ddf96b3fa39897d1773af
MahammadAlishov <mehemmedelishov590@gmail.com>
2021-12-04 12:10:18 +0400

    new Project

---
+++ src/test/java/az/developia/course/qrup28/Qrup28ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.course.qrup28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qrup28ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
