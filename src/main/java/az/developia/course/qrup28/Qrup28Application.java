c9705f1c38c4056567e4cb70d618b70dc690fdd8
Semseddin-Salehli <semseddinsalehli1@gmail.com>
2022-02-04 23:54:59 +0400

    add 2 feign client

--- src/main/java/az/developia/course/qrup28/Qrup28Application.java
+++ src/main/java/az/developia/course/qrup28/Qrup28Application.java
@@ -2,9 +2,11 @@ package az.developia.course.qrup28;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
+@EnableFeignClients
 public class Qrup28Application {
 
     public static void main(String[] args) {
--- src/main/java/az/developia/course/qrup28/configuration/BeanConfig.java
+++ src/main/java/az/developia/course/qrup28/configuration/BeanConfig.java
@@ -1,5 +1,6 @@
 package az.developia.course.qrup28.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,9 @@ public class BeanConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() { return new ObjectMapper();
     }
 }
