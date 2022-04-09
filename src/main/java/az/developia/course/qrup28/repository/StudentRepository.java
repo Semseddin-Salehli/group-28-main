4a95eade6280ded8d87e017c5c03baa5d1408610
MahammadAlishov <mehemmedelishov590@gmail.com>
2022-04-09 10:26:13 +0400

    add new method in repo

--- src/main/java/az/developia/course/qrup28/repository/StudentRepository.java
+++ src/main/java/az/developia/course/qrup28/repository/StudentRepository.java
@@ -1,10 +1,13 @@
 package az.developia.course.qrup28.repository;
 
+import az.developia.course.qrup28.model.SchoolClass;
 import az.developia.course.qrup28.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
@@ -12,4 +15,6 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 
     @Query(value = "select s from Student s where s.id = :studentId ")
     Student getById(@Param("studentId") Long studentId);
+
+    List<Student> findAllBySchoolClass(SchoolClass schoolClass);
 }
